4901f9d5f095045b22c3cc48d98b8d6cd48bcb01
KevinHung <kevinaugust145@gmail.com>
2024-08-02 16:06:50 +0800

    Initial Commit

---
+++ Question/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Question
+//
+//  Created by 洪宗燦 on 2024/8/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ QuestionTests/QuestionTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionTests.swift
+//  QuestionTests
+//
+//  Created by 洪宗燦 on 2024/8/2.
+//
+
+import Testing
+@testable import Question
+
+struct QuestionTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
